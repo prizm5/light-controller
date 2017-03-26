1ffd1c0baf52ef73b50a770c48899da4da1dffb2
Nils Creque <ncreque@us.ibm.com>
2017-03-26 15:50:17 -0400

    correnting api url

--- website/src/utils/LightService.js
+++ website/src/utils/LightService.js
@@ -3,7 +3,7 @@ import React, { PropTypes as T } from 'react'
 
 
   function callApi(state, id, token) {
-    fetch( `${window.location.hostname}:3001/api/protected/toggle`, {
+    fetch( `//${window.location.hostname}:3001/api/protected/toggle`, {
       method: 'POST',
       headers: {
         Authorization: 'Bearer ' + token,
