01168f26b515e7a2c1596ff5141bd659719ca923
Nils Creque <ncreque@us.ibm.com>
2017-03-26 14:19:37 -0400

    adding codesend execute to server api

--- web-api/toggler.js
+++ web-api/toggler.js
@@ -3,12 +3,30 @@ var outlets = require('./outlets.json');
 var codeSendPulseLength = "189";
 var codeSendPIN = "0";
 
+var exec = require('child_process').exec;
+
 var sendCode = function (code) {
-  console.log('./codesend ' + code + ' -p ' + codeSendPIN + ' -l ' + codeSendPulseLength);
+  var cmd = './codesend ' + code + ' -p ' + codeSendPIN + ' -l ' + codeSendPulseLength;
+  console.log(cmd);
+  exec(cmd, function callback(error, stdout, stderr) {
+    if (error) {
+      console.log('Error calling cmd: ', cmd, error);
+    }
+    else if (stderr) {
+      console.log('Error calling cmd stderr: ', cmd, stderr);
+    }
+    else {
+      console.log("cmd success: ", stdout);
+    }
+
+
+    
+  });
+
 }
 
 
-exports.ToggleOn= function(id, state) {
+exports.ToggleOn = function (id, state) {
   var outlet = outlets.filter(function (o) { return o.id == id; })[0];
   console.log(id);
   console.log(outlet[state]);
--- website/server.js
+++ website/server.js
@@ -6,9 +6,9 @@ const indexPath = path.join(__dirname, '/dist/index.html')
 
 const publicPath = express.static(path.join(__dirname, 'dist'))
 
-app.use(express.static(path.join(__dirname, 'dist')));
 app.use('/', publicPath)
 app.get('/index.html', function (_, res) { res.sendFile(indexPath) })
 app.get('/login', function (_, res) { res.sendFile(indexPath) })
+app.get('/home', function (_, res) { res.sendFile(indexPath) })
 app.listen(port)
 console.log(`Listening at http://localhost:${port}`)
