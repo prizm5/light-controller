bd60e6e5b885b521c6c2352c510928311002e48b
Nils Creque <ncreque@us.ibm.com>
2017-03-28 19:32:16 -0400

    updating page header

--- website/src/views/Main/Home/Home.js
+++ website/src/views/Main/Home/Home.js
@@ -1,8 +1,8 @@
 import React, { PropTypes as T } from 'react'
-import {Button, ButtonGroup, Nav, Navbar, NavItem} from 'react-bootstrap'
+import { Button, ButtonGroup, Nav, Navbar, NavItem } from 'react-bootstrap'
 import AuthService from 'utils/AuthService'
 import styles from './styles.module.css'
-import {ToggleButtonGroup} from '../../../Components/ToggleButtonGroup'
+import { ToggleButtonGroup } from '../../../Components/ToggleButtonGroup'
 
 export class Home extends React.Component {
   static contextTypes = {
@@ -19,43 +19,42 @@ export class Home extends React.Component {
       profile: props.auth.getProfile()
     }
     this.token = {
-       token: props.auth.getToken()
+      token: props.auth.getToken()
     }
     props.auth.on('profile_updated', (newProfile) => {
-      this.setState({profile: newProfile})
+      this.setState({ profile: newProfile })
     })
   }
 
-  logout(){
+  logout() {
     this.props.auth.logout()
     this.context.router.push('/login');
   }
 
 
-  render(){
+  render() {
     const { profile } = this.state
     const { token } = this.token
     return (
       <div className={styles.root}>
-          <Navbar inverse collapseOnSelect>
-            <Navbar.Header>
-              <Navbar.Brand>
-                <a href="#">RF Outlets</a>
-              </Navbar.Brand>
-              <Navbar.Toggle />
-            </Navbar.Header>
-              <Nav pullRight>
-                <NavItem eventKey={1} onClick={this.logout.bind(this)}> Welcome {profile.name}! | Logout</NavItem>
-              </Nav>
-          </Navbar> 
-          <div className="container">
-            <ToggleButtonGroup name="Bedroom Light" id="1" token={token} />
-            <ToggleButtonGroup name="Livingroom Light" id="2" token={token} />
-            <ToggleButtonGroup name="Balcony Lights" id="3" token={token} />
-            <ToggleButtonGroup name="TV" id="4" token={token} />
-            <ToggleButtonGroup name="Bedroom Chargers" id="5" token={token} />
-            <ToggleButtonGroup name="All" id="6" token={token} />
-          </div>
+        <Navbar inverse collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">RF Outlets</a>
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Nav pullRight>
+            <NavItem eventKey={1} onClick={this.logout.bind(this)}> Welcome {profile.name}! | Logout</NavItem>
+          </Nav>
+        </Navbar>
+        <div className="container">
+          <ToggleButtonGroup name="Bedroom Light" id="1" token={token} />
+          <ToggleButtonGroup name="Livingroom Light" id="2" token={token} />
+          <ToggleButtonGroup name="Balcony Lights" id="3" token={token} />
+          <ToggleButtonGroup name="TV" id="4" token={token} />
+          <ToggleButtonGroup name="Bedroom Chargers" id="5" token={token} />
+          <ToggleButtonGroup name="All" id="6" token={token} />
+        </div>
       </div>
     )
   }
